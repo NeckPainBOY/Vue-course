89cd5bc049b2c0d6d17cc76f181e16c09bd57249
NeckPainBOY <chenzy204@163.com>
2024-05-23 18:29:11 +0800

    P6-P7

--- dist/vue.js
+++ dist/vue.js
@@ -4,6 +4,135 @@
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Vue = factory());
 })(this, (function () { 'use strict';
 
+  var ncname = "[a-zA-Z_][\\-\\.0-9_a-zA-Z]*";
+  var qnameCapture = "((?:".concat(ncname, "\\:)?").concat(ncname, ")");
+  var startTagOpen = new RegExp("^<".concat(qnameCapture)); // 他匹配到的分组是一个 标签名 <xxxx 匹配到的是开始 标签的名字
+
+  var endTag = new RegExp("^<\\/".concat(qnameCapture, "[^>]*>")); // 匹配的是</xxxx> 最终匹配到的分组就是结束标签的名字
+  var attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
+  // 第一个分组就是属性的 key，value 就是分组3/分组4/分组5
+  var startTagClose = /^\s*(\/?)>/;
+  // console.log(attribute);
+  function parseHTML(html) {
+    var ELEMENT_TYPE = 1;
+    var TEXT_TYPE = 3;
+    var stack = []; // 用于存放元素的
+    var currentParent; // 指向的是栈中的最后一个
+    var root;
+    // 最终需要转化成一颗抽象语法树
+    function createASTElement(tag, attrs) {
+      return {
+        tag: tag,
+        type: ELEMENT_TYPE,
+        children: [],
+        attrs: attrs,
+        parent: null
+      };
+    }
+
+    // 利用栈形结构 来构造一棵树
+    function start(tag, attrs) {
+      var node = createASTElement(tag, attrs); // 创造一个 ast 节点
+      if (!root) {
+        // 看下是否为空树
+        root = node; // 如果为空则当前是树的根节点
+      }
+      if (currentParent) {
+        node.parent = currentParent; // 只赋予了 parent 属性
+        currentParent.children.push(node);
+      }
+      stack.push(node);
+      currentParent = node; // currentParent 为栈中的最后一个
+      // console.log(tag, attrs, "开始");
+    }
+    function chars(text) {
+      text = text.replace(/\s/g, ""); // 如果空格超过两个以上 则删除2个以上的
+      // 文本直接放到当前指向的节点中
+      text && currentParent.children.push({
+        type: TEXT_TYPE,
+        text: text,
+        parent: currentParent
+      });
+      // console.log(text, "文本");
+    }
+    function end(tag) {
+      stack.pop(); // 弹出最后一个
+      currentParent = stack[stack.length - 1];
+      // console.log(tag, "结束");
+    }
+    function advance(n) {
+      html = html.substring(n);
+    }
+    function parseStartTag() {
+      var start = html.match(startTagOpen);
+      // console.log(start); // ['<div', 'div', index: 0, input: '<div id="app">\n  ...... ]
+      if (start) {
+        var match = {
+          tagName: start[1],
+          // 标签名
+          attrs: []
+        };
+        advance(start[0].length);
+        // console.log(html);
+
+        // 如果不是开始标签的结束 就一直匹配下去
+        var attr, _end;
+        while (!(_end = html.match(startTagClose)) && (attr = html.match(attribute))) {
+          // console.log(attr);
+          advance(attr[0].length);
+          match.attrs.push({
+            name: attr[1],
+            value: attr[3] || attr[4] || attr[5] || true
+          });
+        }
+        if (_end) {
+          advance(_end[0].length); // 移除 >
+        }
+        //   console.log(match);
+        return match;
+      }
+      return false;
+    }
+    // html 最开始肯定是一个 div
+    while (html) {
+      // 如果 textEnd 为 0 说明是一个开始标签或者结束标签
+      // 如果 textEnd > 0 说明就是文本的结束位置
+      var textEnd = html.indexOf("<"); // 如果 indexOf 中的索引是 0 则说明是个标签
+      if (textEnd == 0) {
+        var startTagMatch = parseStartTag(); // 开始标签的匹配结果
+        if (startTagMatch) {
+          // 解析到的开始标签
+          // console.log(html);
+          start(startTagMatch.tagName, startTagMatch.attrs);
+          continue;
+        }
+        var endTagMatch = html.match(endTag);
+        if (endTagMatch) {
+          advance(endTagMatch[0].length);
+          end(endTagMatch[1]);
+          continue;
+        }
+      }
+      if (textEnd > 0) {
+        var text = html.substring(0, textEnd); // 文本内容
+        if (text) {
+          chars(text);
+          advance(text.length);
+          // console.log(html);
+        }
+      }
+    }
+    console.log(root);
+  }
+
+  // 对模板进行编译
+  function compileToFunction(template) {
+    // 1. 将 template 转换成 ast 语法树
+    // 2. 生成 render 方法 ( render 方法执行后的放回的结果就是 虚拟 DOM)
+    console.log(template);
+    parseHTML(template);
+  }
+
   function _toPrimitive(t, r) {
     if ("object" != typeof t || !t) return t;
     var e = t[Symbol.toPrimitive];
@@ -199,6 +328,35 @@
 
       // 初始化状态
       initState(vm);
+      if (options.el) {
+        vm.$mount(options.el);
+      }
+    };
+    Vue.prototype.$mount = function (el) {
+      var vm = this;
+      el = document.querySelector(el);
+      var ops = vm.$options;
+      if (!ops.render) {
+        // 先进行查找有没有 render 函数
+        var template; // 没有 render 看一下是否写了 template, 没写 template 采用外部的 template
+        if (!ops.template && el) {
+          // 没有写模板但是写了 el
+          template = el.outerHTML;
+        } else {
+          if (el) {
+            template = ops.template;
+          }
+        }
+        if (template) {
+          var render = compileToFunction(template);
+          ops.render = render; // jsx 最终会被编译成 h('xxx')
+        }
+        // console.log(template);
+      }
+      ops.render; // 最终可以获取 render 方法
+
+      // script 标签引用的 vue.global.js 这个编译过程是在浏览器运行的
+      // runtime 是不包含模板编译的, 整个编译时打包的时候通过 loader 来转义 .vue 文件的，用 runtime 的时候不能使用 template
     };
   }
 
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import { compileToFunction } from "./compiler/index";
 import { initState } from "./state";
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
@@ -9,5 +10,35 @@ export function initMixin(Vue) {
 
     // 初始化状态
     initState(vm);
+    if (options.el) {
+      vm.$mount(options.el);
+    }
+  };
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    el = document.querySelector(el);
+    let ops = vm.$options;
+    if (!ops.render) {
+      // 先进行查找有没有 render 函数
+      let template; // 没有 render 看一下是否写了 template, 没写 template 采用外部的 template
+      if (!ops.template && el) {
+        // 没有写模板但是写了 el
+        template = el.outerHTML;
+      } else {
+        if (el) {
+          template = ops.template;
+        }
+      }
+      if (template) {
+        const render = compileToFunction(template);
+        ops.render = render; // jsx 最终会被编译成 h('xxx')
+      }
+      // console.log(template);
+    }
+    ops.render; // 最终可以获取 render 方法
+
+    // script 标签引用的 vue.global.js 这个编译过程是在浏览器运行的
+    // runtime 是不包含模板编译的, 整个编译时打包的时候通过 loader 来转义 .vue 文件的，用 runtime 的时候不能使用 template
   };
 }
